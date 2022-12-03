6f3a59d5a9a075e83f537781e59df161626e44e3
v <2583695112@qq.com>
2022-12-03 13:56:53 +0800

    feat: add vue template

---
+++ templates/vue/src/main.ts
@@ -0,0 +1,4 @@
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ templates/vue/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    Vue({
+      reactivityTransform: true,
+    }),
+    AutoImport({
+      imports: ["vue", "vue/macros"],
+      dts: "src/types/auto-imports.d.ts",
+    }),
+    Components({
+      dts: "src/types/components.d.ts",
+    })],
+});
